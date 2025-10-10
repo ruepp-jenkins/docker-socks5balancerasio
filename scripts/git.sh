f1079dad19b06eb07bd88c3e1b2e5f32b34b40a6
MyUncleSam <12820919+MyUncleSam@users.noreply.github.com>
2025-10-10 18:22:48 +0200

    use fixed Dockerfile from original repo

--- scripts/git.sh
+++ scripts/git.sh
@@ -3,7 +3,3 @@ set -e
 echo "Fetch remote repository"
 
 git clone https://github.com/Socks5Balancer/Socks5BalancerAsio.git repo
-
-# replace original Dockerfile
-rm repo/Dockerfile
-cp Dockerfile repo/Dockerfile
