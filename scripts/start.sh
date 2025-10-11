f9bc5590f119a431ed77b36b87c0439d7b8d2654
MyUncleSam <12820919+MyUncleSam@users.noreply.github.com>
2025-10-11 14:46:14 +0200

    test

--- scripts/start.sh
+++ scripts/start.sh
@@ -12,14 +12,14 @@ echo "[${BRANCH_NAME}] Building image: ${IMAGE_FULLNAME}"
 if [ "$BRANCH_NAME" = "master" ] || [ "$BRANCH_NAME" = "main" ]
 then
     docker buildx build \
-        --platform linux/amd64,linux/ppc64le,linux/arm/v7,linux/arm64/v8 \
+        --platform linux/amd64,linux/arm/v7,linux/arm64/v8 \
         -t ${IMAGE_FULLNAME}:latest \
         -t ${IMAGE_FULLNAME}:${DATESTAMP} \
         --pull \
         --push ./repo/
 else
     docker buildx build \
-        --platform linux/amd64,linux/ppc64le,linux/arm/v7,linux/arm64/v8 \
+        --platform linux/amd64,linux/arm/v7,linux/arm64/v8 \
         -t ${IMAGE_FULLNAME}-test:${BRANCH_NAME} \
         --pull \
         --push ./repo/
